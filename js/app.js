07561a0818f1a1ed50a588a44ff13f9a0b45891c
AlonaAkimova <aliona.akimova@gmail.com>
2023-02-10 13:48:04 +0700

    warsztat

---
+++ js/app.js
@@ -0,0 +1,42 @@
+function Calculator(form, summary) {
+    this.prices = {
+        products: 0.5,
+        orders: 0.25,
+        package: {
+            basic: 0,
+            professional: 25,
+            premium: 60
+        },
+        accounting: 35,
+        terminal: 5
+    };
+
+this.form = {
+    products: form.querySelector("#products"),
+    orders: form.querySelector("#orders"),
+    package: form.querySelector("#package"),
+    accounting: form.querySelector("#accounting"),
+    terminal: form.querySelector("#terminal")
+};
+
+this.summary = {
+    list: summary.querySelector("ul"),
+    items: summary.querySelector("ul").children,
+    total: {
+        container: summary.querySelector("#total-price"),
+        price: summary.querySelector(".total__price")
+    }
+};
+}
+Calculator.prototype.inputEvent = function (e) {
+    const id = e.currentTarget.id;
+    const value = e.currentTarget.value;
+    const singlePrice = this.prices[id];
+    const totalPrice = value * singlePrice;
+}
+Calculator.prototype.updateSummary = function (id, calc, total, callback) {
+    const summary = this.summary.list.querySelector("[data-id=" + id + "]");
+    const summaryCalc = summary.querySelector(".item__calc");
+    const summaryTotal = summary.querySelector(".item__price");
+};
+
